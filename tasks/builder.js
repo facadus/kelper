8f13ac0f99d2add0b7e67f2e9c0feb0c097f8058
pkotov <pavel.kotov@ctco.lv>
2015-01-19 18:26:36 +0200

    RequireJS package

--- tasks/builder.js
+++ tasks/builder.js
@@ -1,10 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function(grunt){
 
-    grunt.registerTask('builder', 'A sample task that logs stuff.', function(arg1, arg2) {
-        if (arguments.length === 0) {
-            grunt.log.writeln(this.name + ", no args");
-        } else {
-            grunt.log.writeln(this.name + ", " + arg1 + " " + arg2);
+    grunt.registerTask('builder', 'Build task', function(){
+        var config = {};
+        config.builder = grunt.config.get("builder");
+        config.optiomization = grunt.config.get("builder");
+
+        var cwd = process.cwd();
+
+        if(typeof config.builder != "undefined"){
+            if(typeof config.builder.requirejs != "undefined"){
+
+                // Loading RequireJS if needed -> Start
+                process.chdir(__dirname + "/../");
+                grunt.loadNpmTasks("grunt-contrib-requirejs");
+                process.chdir(cwd);
+                // Loading -> End
+
+                // requirejs.config = grunt.file.readJSON(config.builder.requirejs);
+
+                grunt.task.run(["requirejs"]);
+            }
+
         }
+
+        if(typeof config.optiomization != "undefined"){
+
+        }
+
+
+        grunt.log.writeln(this.name + " is running");
     });
+
 };

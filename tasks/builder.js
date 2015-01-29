a3a40016d8bb8f5783b4b1fe1bae2d9d5080ccdd
pkotov <pavel.kotov@ctco.lv>
2015-01-29 09:34:16 +0200

    Compile process - Ready

--- tasks/builder.js
+++ tasks/builder.js
@@ -1,27 +1,26 @@
 module.exports = function(grunt){
-    var builder = require("./modules/builder").init(grunt);
-    var compile = require("./modules/compile").init(grunt);
-    var optimizer = require("./modules/optimizer").init(grunt);
+
+    var path = require('path');
+    var configuration = {
+        builderPath: path.normalize(__dirname),
+        modulePath: path.normalize(__dirname) + path.sep + "modules",
+        modules: [
+            "compile",
+            "optimization",
+            "finalization"
+        ]
+    };
 
     grunt.registerTask('builder', 'Build task', function(){
-        var config = {};
-        config.compile = grunt.config.get("compile");
-        config.builder = grunt.config.get("builder");
-        config.optimization = grunt.config.get("optimization");
 
-        if(typeof config.compile != "undefined"){
-            compile.run(config.compile);
-        }
+        // Running all modules
+        configuration.modules.forEach(function(module){
+            module = require(configuration.modulePath + path.sep + module + path.sep + "main").init(grunt);
+            module.modulePath = path.dirname(configuration.builderPath);
+            module.run();
+        });
 
-        if(typeof config.builder != "undefined"){
-            builder.run(config.builder);
-        }
-
-        if(typeof config.optimization != "undefined"){
-            optimizer.run(config.optimization);
-        }
-
-        grunt.log.writeln(this.name + " is running");
     });
 
+    grunt.registerTask("default", ["builder"]);
 };
---
+++ tasks/modules/compile/main.js
@@ -0,0 +1,87 @@
+//External modules
+var path = require('path');
+
+// Module Compile
+exports.init = function(grunt){
+    return {
+        name: path.basename(__dirname),
+        run: function(){
+            var configuration = {};
+
+            // Load default configuration
+            if(grunt.file.exists(__dirname + path.sep + "config" + path.sep + "default.json")){
+                try{
+                    configuration = grunt.file.readJSON(__dirname + path.sep + "config" + path.sep + "default.json");
+                    grunt.log.writeln(this.name + " plugin default configuration is loaded!");
+                }catch(ex){
+                    grunt.log.writeln("[ERROR] " + this.name + " plugin default configuration has error!");
+                    configuration = {};
+                }
+            }
+
+            // Load user created configuration
+            if(grunt.file.exists(process.cwd() + path.sep + "config" + path.sep + "build" + path.sep + this.name + ".js")){
+                var config = require(process.cwd() + path.sep + "config" + path.sep + "build" + path.sep + this.name + ".js")(grunt);
+
+                //Parsing configuration
+                configuration = this.mergeObjects(configuration, this.parse(config));
+            }else{
+                grunt.log.writeln(this.name + " user configuration not found, continue");
+            }
+
+            grunt.config.set("typescript", configuration);
+            this.loadPlugin("grunt-typescript");
+
+            grunt.task.run("typescript");
+        },
+        loadPlugin: function(pluginName){
+            console.log(this.modulePath);
+            var cwd = process.cwd();
+            process.chdir(this.modulePath);
+            grunt.loadNpmTasks(pluginName);
+            process.chdir(cwd);
+        },
+        parse: function(configuration){
+            // Parsing
+            if(configuration.hasOwnProperty("source")){
+                configuration.src = process.cwd() + path.sep + path.normalize(configuration.source) + path.sep + "**" + path.sep + "*.ts";
+                delete configuration.source;
+            }
+            if(configuration.hasOwnProperty("target")){
+                configuration.dest = process.cwd() + path.sep + path.normalize(configuration.target);
+                delete configuration.target;
+            }
+            if(configuration.hasOwnProperty("version")){
+                if(configuration.hasOwnProperty("options")){
+                    configuration.options.target = configuration.version;
+                }else{
+                    configuration.options = {target: configuration.version};
+                }
+                delete configuration.version;
+            }
+
+            // Fix for TypeScript
+            return {
+                base: configuration
+            };
+        },
+        mergeObjects: function(){
+            if(arguments.length > 1){
+                var destination = arguments[0];
+                for(var i = 1; i < arguments.length; i++){
+                    var source = arguments[i];
+                    for (var property in source){
+                        if(grunt.util.kindOf(destination[property]) == "object" && grunt.util.kindOf(source[property]) == "object") {
+                            destination[property] = destination[property] || {};
+                            arguments.callee(destination[property], source[property]);
+                        } else {
+                            destination[property] = source[property];
+                        }
+                    }
+                    return destination;
+                }
+            }
+            return destination[0] || {};
+        }
+    }
+};

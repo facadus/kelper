09fe52e06249965a7f7c6014779fb192fdbd9a13
pkotov <pavel.kotov@ctco.lv>
2015-03-02 14:23:53 +0200

    Package dependencies

--- tasks/modules/compile/include/dependencies.js
+++ tasks/modules/compile/include/dependencies.js
@@ -44,7 +44,7 @@ exports.init = function(grunt){
                 var files = grunt.file.expand({
                     cwd: path.dirname(file)
                 }, dependencies[deps]).map(function(iter){
-                    return '"./' + iter.replace(".js", "") + '"';
+                    return './' + iter.replace(".js", "");
                 });
 
                 // Start replacing dependencies
@@ -54,8 +54,31 @@ exports.init = function(grunt){
 
                 // Find Dependencies ^
                 if(result){
+                    var regEx = /"([\w\/.]+)"/gm;
+                    var deps = {};
+
+                    // Parse file's components
+                    while((m = regEx.exec(result[1])) != null){
+                        if(m.index === regEx.lastIndex){
+                            re.lastIndex++;
+                        }
+                        deps[path.resolve(filePath, m[1])] = m[1];
+                    }
+
+                    // Parse additional components
+                    files.forEach(function(file){
+                        var filePathed = path.resolve(filePath, file);
+                        if(deps[filePathed] == undefined){
+                            deps[filePathed] = file;
+                        }
+                    });
+
                     // Calculate edits
-                    var fixedPart = result[1] + ", " + files.join(', ');
+                    var fixedPart = [];
+                    for(var key in deps){
+                        fixedPart.push('"' + deps[key] + '"');
+                    }
+                    fixedPart = fixedPart.join(", ");
                     var replace = result[0].replace(result[1], fixedPart);
 
                     // Replace file data

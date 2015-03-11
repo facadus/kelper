69e4051784f9dcb3c46f20b6d55408ef2003737b
pkotov <pavel.kotov@ctco.lv>
2015-03-11 09:55:28 +0200

    Install plugin - unfinished

--- bin/install.js
+++ bin/install.js
@@ -1,6 +1,36 @@
 #!/usr/bin/env node
 
-var path = require("path");
 var fs = require("fs");
 
-console.log(process.cwd());
+function readPackageJson(dependency) {
+    var path = require("path");
+    if (dependency) {
+        dependency = "node_modules/" + dependency;
+    } else {
+        dependency = "";
+    }
+
+    try {
+        var config = fs.readFileSync(path.resolve(process.cwd(), dependency, "package.json"), "utf8");
+        try {
+            return JSON.parse(config);
+        } catch (ex) {
+            console.log("File `package.json` can not be parsed!");
+        }
+    } catch (ex) {
+        console.log("File `package.json` was not found!");
+    }
+
+    return false;
+}
+
+var config = readPackageJson();
+
+if (config && config.dependencies) {
+    for (var deps in config.dependencies) {
+        var dependencyPackage = readPackageJson(deps);
+        if (dependencyPackage && dependencyPackage.moduleType && dependencyPackage.moduleType.toUpperCase() == "AMD") {
+            console.log("Found module to copy : " + deps);
+        }
+    }
+}

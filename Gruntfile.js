6777ccdcc76f684b305a2e354c0436b974b5968d
Alexander Domotenko <alexander.domotenko@ctco.lv>
2015-03-11 11:21:16 +0200

    publishConfig added to package.json grunt-bump added for versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,35 @@
 var path = require("path");
 
 module.exports = function (grunt) {
-    grunt.registerTask("test", "Build task test", function(){
-        var done = this.async();
-        require('child_process').exec("mocha " + path.resolve(__dirname, "test/kelper.test.js"), function(err, stdout){
-            grunt.log.write(stdout);
-            done(err);
-        });
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: false,
+                push: false,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
+        },
+        exec: {
+            pack: {
+                cmd: 'npm pack'
+            },
+            test: {
+                cmd: 'mocha ' + path.resolve(__dirname, 'test/kelper.test.js')
+            }
+        }
     });
-    grunt.registerTask("default", ["test"]);
+
+    grunt.registerTask("test", ["exec:test"]);
+    grunt.registerTask('patch', ['bump:patch', 'default', 'exec:pack']);
+
 };

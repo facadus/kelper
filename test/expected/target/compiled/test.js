63a3ba8175fd445a491807366237e5ca604e44a3
domotenko <domotenko@users.noreply.github.com>
2017-08-10 09:30:13 +0300

    Update test.js
    typescript 2.4: test fixed

--- test/expected/target/compiled/test.js
+++ test/expected/target/compiled/test.js
@@ -1,5 +1,6 @@
 define(["require", "exports", "common/main"], function (require, exports, Common) {
     "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
     exports.greeter = new Common("world");
     exports.greeter.greet();
 });
